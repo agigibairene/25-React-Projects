04f42c5687a6476293e39be41891caf3f4f84a9e
agigibairene <116544268+agigibairene@users.noreply.github.com>
2024-05-07 10:49:31 +0200

    Delete Investment-Calculator directory

--- Investment-Calculator/src/Components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import investmentImage from "../assets/investment-calculator-logo.png"
-export default function Header(){
-    return (
-        <div id="header">
-            <img src={investmentImage} alt="image" />
-            <h1>Investment Calculator</h1>
-        </div>
-    )
-}
